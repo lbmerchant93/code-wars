fc06c3c02924c440809091777a8b8653e0481381
Lucas Merchant <lbmerchant93@gmail.com>
2023-04-24 13:05:51 -0600

    Solve quadrants code challenge, pass test

---
+++ src/quadrants.js
@@ -0,0 +1,25 @@
+// Task
+// Given a point in a Euclidean plane (x and y), return the quadrant the point exists in: 1, 2, 3 or 4 (integer). x and y are non-zero integers, therefore the given point never lies on the axes.
+
+// Examples
+// (1, 2)     => 1
+// (3, 5)     => 1
+// (-10, 100) => 2
+// (-1, -9)   => 3
+// (19, -56)  => 4
+
+function quadrant(x, y) {
+    if (Math.sign(x) === +1 && Math.sign(y) === +1) {
+      return 1
+    } else if (Math.sign(x) === -1 && Math.sign(y) === -1) {
+      return 3
+    } else if (Math.sign(x) === -1 && Math.sign(y) === +1) {
+      return 2
+    } else if (Math.sign(x) === +1 && Math.sign(y) === -1) {
+      return 4
+    }
+}
+
+module.exports = {
+    quadrant
+}
--- test/remove-the-time-test.js
+++ test/remove-the-time-test.js
@@ -2,7 +2,7 @@ const chai = require("chai");
 const expect = chai.expect;
 const { shortenToDate } = require('../src/remove-the-time');
 
-describe("Tests", () => {
+describe("Remove-The-Time", () => {
     it("test", () => {
         expect(shortenToDate("Friday May 2, 9am")).to.deep.equal("Friday May 2");
         expect(shortenToDate("Tuesday January 29, 10pm")).to.deep.equal("Tuesday January 29");
